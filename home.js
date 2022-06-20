9dfe26031b3a509841a467958843573339dc66bf
man1521 <manushka90@mail.ru>
2022-06-21 00:17:40 +0400

    my 1st pro

---
+++ home.js
@@ -0,0 +1,11 @@
+"use strict";
+
+ const project ={
+     name:"shcool N187",
+     num:"school N104",
+     dates() 
+     { console.log(this.name);
+    console.log(this.num);
+}
+ };
+ project.dates();
---
+++ orinak.js
@@ -0,0 +1,17 @@
+if(app.restaurantDetails[7].isOpen ==false){
+    console.log("Ներեցեք, այս ժամին չենք սպասարկում");}
+else 
+    console.log("Բարև Ձեզ, Ձեզ սպասարկում է  ${app.restaurantStaff[13].customer} ը")
+console.log("Մեր հասցեն է " + app.restaurantDetails[0].address);
+console.log("Մեր հեռախոսահամարն է " + app.restaurantDetails[1].telNumber);
+
+if(isDelivery = app.restaurantDetails[6].isDelivery == true)
+{console.log("Այո, կարող ենք իրականացնել Ձեր պատվերը")};
+else
+ console.log("Կներեք, բայց մեր առաքիչը զբաղված է, պետք է դուք մոտենաք");
+
+
+ const client=x;
+     y= "ունե՞ք գարեջուր";
+ if (x==y) {
+    console.log("էլ չզանգեք ։)"+ app.restaurantMenu[2] )};
