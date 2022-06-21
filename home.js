1fc776b67652d3bd760c108b9f86e9eef7970221
Mane Mkhitaryan <107885751+man1521@users.noreply.github.com>
2022-06-21 15:30:31 +0400

    my homework

--- home.js
+++ home.js
@@ -2,58 +2,65 @@
 
 //for 
 //1
-// let i = 0;
-// for (i=1; i<10 ;i++)
-// {
-// console.log(i);
-// }
+let i = 0;
+for (i=1; i<10 ;i++)
+{
+console.log(i);
+}
 //2
-// let str = "Mane";
-// for (let i = 0; i < str.length; i++) {
-//   console.log(str[i]);
-// }
+let str = "Mane";
+for (let i = 0; i < str.length; i++) {
+  console.log(str[i]);
+}
 //3
-// let fruit = ["banana", "apple", "kivi", "orange"];
-// for(let i = 0; i < fruit.length; i++){ 
-// console.log(fruit[i]);
-// }
-//3
-// let i=20;
-// for(i=0;i<20;i+=2) {
-    
-//     if(i===16){
-//         continue;
-//     }
-// console.log(i);
-// }
+let fruit = ["banana", "apple", "kivi", "orange"];
+for(let i = 0; i < fruit.length; i++){ 
+console.log(fruit[i]);
+}
 //4
-// let i=10;
-// let x="hello";
-// for(let i=0;i<=10;i++){
-//    console.log(i) ; 
-//    console.log("yes")
-// console.log(x);
-// if(i===7){
-//    break;
-// }; 
-// };
+let i=20;
+for(i=0;i<20;i+=2) {
+    
+    if(i===16){
+        continue;
+    }
+console.log(i);
+}
 //5
-// let x="stop";
-// for(let i=30;i>=5;i--)
-// {console.log(i);
-//     if(i===20){console.log(x);
-//         continue;}  
+let i=10;
+let x="hello";
+for(let i=0;i<=10;i++){
+   console.log(i) ; 
+   console.log("yes")
+console.log(x);
+if(i===7){
+   break;
+}; 
+};
+//6
+let x="stop";
+for(let i=30;i>=5;i--)
+{console.log(i);
+    if(i===20){console.log(x);
+        continue;}  
 
-// }
+}
 
 // while 1
 
-// let i=1, n=15;
-// while (i<=n) {
-//     console.log(i);
-//     i++;
+let i=1, n=15;
+while (i<=n) {
+    console.log(i);
+    i++;
            
-//     };
+    };
+     //while 2
+     let i=0;
+     
+     while (i<10){
+        console.log(" the number is "+i);
+        i++;
+     }
 
     //do while
      let count= 0 
